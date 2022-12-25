b79d52eba3cac087c433f9b86822020dfe91b9ea
YerldSHO <yakovec.aleksandr@bk.ru>
2022-12-25 19:30:42 +1000

    nine_pr

--- web_django/settings.py
+++ web_django/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'myproject2',
         'USER': 'root',
         'PASSWORD': 'admin',
-        'HOST': '172.17.0.2',
+        'HOST': 'db',
         'PORT': '3306',
     }
 }
