f4a83d5f6286e78ea6a8306c5d0406f3d3b894d4
YerldSHO <yakovec.aleksandr@bk.ru>
2023-01-15 19:13:45 +1000

    final

--- web_django/settings.py
+++ web_django/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'myproject2',
         'USER': 'root',
         'PASSWORD': 'admin',
-        'HOST': '172.17.0.2',
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
